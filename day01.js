5645b989dc63f5e9cf83caac73513bf21069de62
ganesh-j-m <ganeshmore1552@gmail.com>
2025-10-14 03:14:33 -0700

    Built in Methods

--- day01.js
+++ day01.js
@@ -1,9 +1,64 @@
-// Map Function In Javascript
+// // Map Function In Javascript
 
-let arr = [10,20,30];
+// let arr = [10, 20, 30];
 
-let ansArray = arr.map((number)=>{
-    return number*number;
+// let ansArray = arr.map((number) => {
+//     return number * number;
+// })
+
+// console.log(ansArray);
+
+//  Ex- 2) Using Map Function
+
+// arr = [10, 20, 30]
+// let ansArray = arr.map((arr) => {
+//     console.log(arr * 10);
+// })
+
+// Ex-3) Print The Index Using .Map Function
+
+// arr = [10, 100, 1000]
+// let print = arr.map((Num, Index) => {
+//     console.log(Num, Index);
+// })
+
+// Filter Method In Javascript 
+
+// let arr = [10, 20, 30, 40, 41, 42, 43, 44]
+// let ans = arr.filter((num) => {
+//     if (num % 2 == 1) {
+//         return true;
+//     }
+// });
+
+// console.log(ans);
+
+// Short Method(filter)
+
+// let arr = [10, 20, 30, 40, 41, 42, 43, 44]
+// let ans = arr.filter((num) => {
+//     return num < 25;
+// });
+
+// console.log(ans);
+
+// Ex- 2)
+
+// let arr = [1,2,3,4,5]
+// let ans = arr.filter((num)=>{
+//     return num < 4;
+// });
+
+// console.log(ans);
+
+let arr = [1, 2, 'love', 'ganu', 3, 4]
+let ans = arr.filter((value) => {
+    if (typeof (value) == 'string') {
+        return true;
+    }
+    else {
+        return false;
+    };
 });
 
-console.log(ansArray);
+console.log(ans);
---
+++ day02.js
@@ -0,0 +1,37 @@
+// today I am Learning Sort Method
+
+// let arr = [1, 9, 7, 5, 4, 1, 3, 7, 8];
+// arr.sort();
+// console.log(arr.indexOf(2));
+
+//  For Each Loop ow to work 
+
+// let arr = [10, 20, 30]
+// let length = arr.length;
+// console.log("length: ", length);
+
+// How to get Index and Value Of the Array
+
+// let arr = [10, 20, 30]
+// let ans = arr.forEach((length, index) => {
+//     console.log("Length: ", length, "Index: ", index)
+// });
+
+// 2) FOR IN 
+
+// let obj = {
+//     name: "ganu",
+//     surename: "more",
+//     role: "Fronted Developer"
+// };
+
+// for (let key in obj) {
+//     console.log("key: ", key, "value: ", obj);
+// };
+
+//3) FOR OF 
+
+let arr = [10, 20, 30]
+for (let value of arr) {
+    console.log(value);
+}

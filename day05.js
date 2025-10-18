dad13004965aafc510abaf5b098a3c1a8479ba0a
ganesh-j-m <ganeshmore1552@gmail.com>
2025-10-18 02:40:21 -0700

    Global Scope,Function Scope, Block Scope, & temporal dead zone

---
+++ day05.js
@@ -0,0 +1,23 @@
+// // Function variable 
+
+// function sayHello() {
+//     var fullName = "Ganu";
+//     console.log("hello Dunia",);
+// }
+
+// sayHello(fullName);
+
+// Block Scope variable
+
+// console.log("height", height);
+
+{
+    var height = 40;
+}
+
+// console.log(height);
+
+function details() {
+    console.log("function height", height);
+}
+details();

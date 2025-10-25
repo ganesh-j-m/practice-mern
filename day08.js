d71726bdcd350428ae75c1635e426086faa7b894
ganesh-j-m <ganeshmore1552@gmail.com>
2025-10-25 02:32:05 -0700

    DOM in js

--- day08.js
+++ day08.js
@@ -1,23 +1,5 @@
-// Error handling
+// Access  Element
 
-// 1) Compile time error
-// 2) run time error
+let accHero = document.getElementsByClassName("hero");
 
-// console.log(x)
-
-// try {
-//     // console.log(x);
-//     console.log("try block starts here");
-
-// } catch (error) {
-//     console.log("i am inside catch block")
-// }
-
-// How to generate error 
-
-try {
-    console.log(x);
-}
-catch (error) {
-    throw new Error("bhai tu pahile khana kha ke aa", error);
-}
+console.log(accHero);
---
+++ script.js
@@ -0,0 +1,54 @@
+// console.log("hello dada");
+// let x = document.querySelector("h1");
+// console.log(x);
+
+// let name = document.querySelector(".yes");
+
+// name.style.backgroundColor = "red";
+// name.style.color = "white";
+// name.style.fontSize = "4rem";
+// name.style.textAlign = "center";
+
+// let head = document.querySelector(".dada");
+
+// head.style.backgroundColor = "black";
+// head.style.color = "white";
+
+// let fh = document.querySelector(".fh");
+// console.log(fh);
+
+// fh.style.backgroundColor = "red";
+// fh.style.color = "white";
+// fh.style.textAlign = "center";
+// fh.style.padding = "2rem"
+
+// let boxes = document.querySelector(".boxes");
+// console.log("boxes");
+
+// boxes.style.height = "2rem";
+// boxes.style.color = "red";
+// boxes.style.backgroundColor = "yellow";
+
+
+// let fh = document.querySelector(".fh");
+
+// fh.innerHTML = "changed";
+// fh.innerHTML = "mai yeh aasani se badal skata hnu";
+// fh.innerHTML = "how are you bro";
+
+let boxes = document.querySelector(".boxes");
+boxes.style.backgroundColor = ("red");
+
+let onlyBox = document.querySelector(".box");
+onlyBox.style.color = ("white");
+onlyBox.style.height = ("2rem");
+
+onlyBox.innerHTML = "hello";
+
+setTimeout(function () {
+    onlyBox.innerHTML = "lavdya"
+}, 2000)
+
+setTimeout(function () {
+    onlyBox.innerHTML = "MC"
+}, 4000)

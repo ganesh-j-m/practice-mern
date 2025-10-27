a6591c30750d1a895ae6d114322b6452bafd1da2
ganesh-j-m <ganeshmore1552@gmail.com>
2025-10-27 03:01:00 -0700

    all about event listener 2)event loop 3)promise

---
+++ day10.js
@@ -0,0 +1,17 @@
+// console.log("start");
+// function saytimeOut() {
+//     console.log("waiting");
+// }
+// setTimeout(saytimeOut, 5000);
+
+// console.log("end");
+
+
+const datas = [{
+    name: "ganesh",
+    profession: "student"
+},
+{
+    name: "kumar",
+    profession: "front-end developer"
+}]
